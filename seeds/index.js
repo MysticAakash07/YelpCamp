49ca37fe03b47e9d463c897f20f9f68839adb08a
MysticAakash07 <rahulsagar460@gmail.com>
2024-11-13 21:06:50 +0530

    Created new files named schemas.js error.js catchAsync.js ExpressError.js with partials and layouts folders and added styling and error handling

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async()=>{
     await Campground.deleteMany({});
     for(let i = 0; i < 50 ; i++){
         const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground({
             title:`${sample(descriptors)} ${sample(places)}`,
-            location:`${cities[random1000].city},${cities[random1000].state}`
+            location:`${cities[random1000].city},${cities[random1000].state}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus necessitatibus modi veniam, dolore laborum sequi officiis in adipisci veritatis, quod nisi dignissimos soluta. Minus nulla vel excepturi, voluptatum commodi dolores.',
+            price  
         })
         await camp.save()
     }
