9f1f1882784be6798f03a7681bfe7b49020f5183
MysticAakash07 <rahulsagar460@gmail.com>
2024-11-18 16:03:44 +0530

    Added review post route and functionality

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const ejsMate = require('ejs-mate');
+
 const {campgroundSchema} = require('./schemas.js')
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
+const Review = require('./models/review.js');
 const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
@@ -74,6 +76,15 @@ app.delete('/campgrounds/:id',catchAsync( async(req,res)=>{
     const{id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', catchAsync( async (req, res)=>{
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 app.all('*', (req, res, next)=>{
