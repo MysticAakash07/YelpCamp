d14d1cbfa99075dcbdcb3e4243a66895e9ff9a20
MysticAakash07 <rahulsagar460@gmail.com>
2024-11-25 15:18:53 +0530

    Added functionality to remember a route prior to login

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const router = express.Router();
+const {storeReturnTo} = require('../middleware');
 
 router.get('/register',(req, res)=>{
     res.render('users/register');
@@ -12,8 +13,11 @@ router.post('/register',catchAsync(async (req, res)=>{
         const{email, username , password} = req.body;
         const user = new User({email, username});
         const registeredUser = await User.register(user,password);
-        req.flash('success','Welcome to Yelp Camp!');
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err=>{
+            if(err) return next(err);
+            req.flash('success','Welcome to Yelp Camp!');
+            res.redirect('/campgrounds');
+        });
     }catch(e){
         req.flash('error',e.message);
         res.redirect('/register');
@@ -24,9 +28,10 @@ router.get('/login',(req, res)=>{
     res.render('users/login');
 })
 
-router.post('/login', passport.authenticate('local',{failureFlash:true, failureRedirect:'/login'}),(req, res)=>{
-    req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds'); 
+router.post('/login',storeReturnTo, passport.authenticate('local',{failureFlash:true, failureRedirect:'/login'}), (req, res)=>{
+    req.flash('success', `Welcome back! ${req.user.username}`);
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
+    res.redirect(redirectUrl); 
 })
 
 router.get('/logout', (req, res, next) => {
