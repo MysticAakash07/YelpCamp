63675c16b6984d805838ea4b7390024ac5d55bcf
MysticAakash07 <rahulsagar460@gmail.com>
2024-11-26 20:09:43 +0530

    Modified new campground form

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,11 +4,18 @@ const campgrounds = require('../controllers/campgrounds')
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
+const multer = require('multer');
+const upload = multer({dest:'uploads/'});
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn , validateCampground, catchAsync( campgrounds.createCampground));
-
+    // .post(isLoggedIn , validateCampground, catchAsync( campgrounds.createCampground));
+    .post(upload.array('image'),(req, res)=>{
+        res.send({
+            body: req.body,
+            file: req.files
+        });
+    })
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
