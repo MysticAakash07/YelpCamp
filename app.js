114bff3e2bdc79e60708e70a57ed9caa3f328f27
MysticAakash07 <rahulsagar460@gmail.com>
2024-12-04 01:13:32 +0530

    Added express-mongo-sanitize to prevent mongoInjection

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require('path');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -37,7 +38,7 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')));
-
+app.use(mongoSanitize());
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret',
@@ -60,6 +61,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser())
 
 app.use((req,  res, next)=>{
+    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success') || '';
     res.locals.error = req.flash('error');
