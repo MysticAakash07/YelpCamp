d8a484f6a7dfa1b2d923bb69269920714627e2dc
MysticAakash07 <rahulsagar460@gmail.com>
2024-12-04 19:23:01 +0530

    Added and configured helmet for security

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -54,6 +55,45 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/", 
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/", 
+];
+const connectSrcUrls = [
+    "https://api.maptiler.com/",
+];
+
+app.use(helmet.contentSecurityPolicy({
+    directives:{
+        defaultSrc: [],
+        connectSrc: ["'self'",...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc:["'self'","blob:"],
+        objectSrc: [],
+        imgSrc: [
+            "'self'",
+            "'blob:'",
+            "data:",
+            "https://res.cloudinary.com/dfmdqbghk/",         
+            "https://api.maptiler.com/",
+        ],
+    }
+}))
+
 
 app.use(passport.initialize());
 app.use(passport.session())
